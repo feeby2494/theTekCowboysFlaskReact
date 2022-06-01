ee27f06688673a10fabe8e8938f253a27fd418c0
Jamie Lynn <toby2494@hotmail.com>
2022-05-31 21:37:37 -0500

    Set up routes for the general ledger. Still need the DB quarries. Goal is to make this as simple as possible. One to One and One to Many only!
    Committing so I can use the Macbook from here on out.

--- api/api/general_ledger/routes.py
+++ api/api/general_ledger/routes.py
@@ -7,14 +7,30 @@ from api.jwt_token.__token_required__ import token_required
 import datetime
 
 
-@app.route('/api/general_ledger', methods=['GET', 'POST'])
+@app.route('/api/general_ledger_all', methods=['GET'])
 def general_ledger():
 
     if request.method == "GET":
 
         return Response(json.dumps({"message": "hi"}), mimetype='application/json')
     
+ 
+ @app.route('/api/general_ledger_one', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def general_ledger():
+
     if request.method == "GET":
+
+        return Response(json.dumps({"message": "hi"}), mimetype='application/json')
+
+    if request.method == "POST":
+
+        return Response(json.dumps({"message": "hi"}), mimetype='application/json')
+
+    if request.method == "PUT":
+
+        return Response(json.dumps({"message": "hi"}), mimetype='application/json')
+
+    if request.method == "DELETE":
 
         return Response(json.dumps({"message": "hi"}), mimetype='application/json')
  
