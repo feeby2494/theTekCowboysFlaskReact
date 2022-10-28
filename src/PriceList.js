0aec93e32ce0e3acdc912c08a1afb407bffbacb9
feeby2494 <toby2494@hotmail.com>
2022-10-28 16:22:13 -0500

    Made a json file with prices and have this populating the tables and nav links on PriceList component.
    Will get 10 devices for each brand, Samsung and Apple, then get some main prices. Will add two pictures of
    me doing a repair and my work bench.
    
    Will comment out Web service for now.

--- src/PriceList.js
+++ src/PriceList.js
@@ -3,18 +3,22 @@ import { Accordion, Card, Button, Tabs, Tab, Nav, Col, Row, Sonnet, Container, N
 import { withCollapsableContainer } from './home/withCollapsableContainer';
 
 
+
+
 export const PriceList = () => {
     const [showSamsung, setShowSamsung] = useState(false);
     const [showApple, setShowApple] = useState(false);
     const handleShowSamsung = () => setShowSamsung(!showSamsung);
     const handleShowApple = () => setShowApple(!showApple);
+    const priceListJson = require('./priceList.json');
+    // const [priceList, setPriceList] = useState(priceListJson);
     
     
     return (
         <Container>
 
-            <SamsungDevices componentTitle="Samsung Devices" button_text="Show Samsung Prices" id_name="samsung-prices" showContent={showSamsung} handleShowContent={handleShowSamsung} />
-            <AppleDevices componentTitle="Apple Devices" button_text="Show Apple Prices" id_name="apple-prices" showContent={showApple} handleShowContent={handleShowApple} />
+            <SamsungDevices priceListJson={priceListJson} componentTitle="Samsung Devices" button_text="Show Samsung Prices" id_name="samsung-prices" showContent={showSamsung} handleShowContent={handleShowSamsung} />
+            <AppleDevices priceListJson={priceListJson} componentTitle="Apple Devices" button_text="Show Apple Prices" id_name="apple-prices" showContent={showApple} handleShowContent={handleShowApple} />
 
         </Container>
     );
@@ -24,19 +28,51 @@ export const PriceList = () => {
 const AppleDevices = withCollapsableContainer((props) => {
     return (
         <Tab.Container id="left-tabs-example" defaultActiveKey="first" className="mt-3">
-  
             <Col sm={3} className="mt-3">
             <Nav variant="pills" className="flex-column">
                 <NavDropdown title="Apple Prices" id="basic-nav-dropdown">
                     <input type="text" />
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item eventKey="first">iPad 10.5 Pro</NavDropdown.Item>
-                    <NavDropdown.Item eventKey="second">Macbook Air 2013-2017 A1466</NavDropdown.Item>
+                    {
+                        props.priceListJson["apple_devices"].map((dev) => {
+                            return (
+                                <NavDropdown.Item eventKey={dev.name}>{dev.name}</NavDropdown.Item>
+                            );
+                        })
+                    }
                 </NavDropdown>      
             </Nav>
             </Col>
             <Col sm={9} className="mt-3">
             <Tab.Content>
+                {
+                    props.priceListJson["apple_devices"].map((dev) => {
+                        return (
+                            <Tab.Pane eventKey={dev.name}>
+                            <h5>{dev.name}</h5>
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                    <th>Repair</th>
+                                    <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        Object.entries(dev.repairs).map((repair) => {
+                                            return (
+                                                <tr>
+                                                    <td>{repair[0]}</td>
+                                                    <td>{repair[1]}</td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+                                </tbody>
+                            </Table>
+                        </Tab.Pane>
+                        );
+                    })
+                }
                 <Tab.Pane eventKey="first">
                     <h5>iPad 10.5 Pro</h5>
                     <Table striped bordered hover size="sm">
@@ -75,26 +111,49 @@ const SamsungDevices = withCollapsableContainer((props) => {
   
             <Col sm={3} className="mt-3">
             <Nav variant="pills" className="flex-column">
-                <NavDropdown title="Travel Preparation" id="basic-nav-dropdown">
+                <NavDropdown title="Samsung Prices" id="basic-nav-dropdown">
                     <input type="text" />
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item eventKey="first">Galaxy S8</NavDropdown.Item>
-                    <NavDropdown.Item eventKey="second">Galaxy S21</NavDropdown.Item>
+                    {
+                        props.priceListJson["samsung_devices"].map((dev) => {
+                            return (
+                                <NavDropdown.Item eventKey={dev.name}>{dev.name}</NavDropdown.Item>
+                            );
+                        })
+                    }
                 </NavDropdown>      
             </Nav>
             </Col>
             <Col sm={9} className="mt-3">
             <Tab.Content>
-                <Tab.Pane eventKey="first">
-                <div>
-                    LCD/Glass: $150
-                </div>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">
-                    <div>
-                    LCD: $150
-                </div>
-                </Tab.Pane>
+                {
+                    props.priceListJson["samsung_devices"].map((dev) => {
+                        return (
+                            <Tab.Pane eventKey={dev.name}>
+                            <h5>{dev.name}</h5>
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                    <th>Repair</th>
+                                    <th>Price</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        Object.entries(dev.repairs).map((repair) => {
+                                            return (
+                                                <tr>
+                                                    <td>{repair[0]}</td>
+                                                    <td>{repair[1]}</td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
+                                </tbody>
+                            </Table>
+                        </Tab.Pane>
+                        );
+                    })
+                }
             </Tab.Content>
             </Col>
         </Tab.Container>
